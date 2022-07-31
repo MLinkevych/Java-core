4732e99a53fe49a2f1f90426076eab4c92142182
Maksym Linkevych <95864524+MLinkevych@users.noreply.github.com>
2022-07-31 22:15:40 +0300

    new homework

---
+++ src/homework4_2/Bird.java
@@ -0,0 +1,30 @@
+package homework4_2;
+
+public abstract class Bird {
+    private String feathers;
+    private String layEggs;
+
+    public String getFeathers() {
+        return feathers;
+    }
+
+    public String getLayEggs() {
+        return layEggs;
+    }
+
+    public void setFeathers(String feathers) {
+        this.feathers = feathers;
+    }
+
+    public void setLayEggs(String layEggs) {
+        this.layEggs = layEggs;
+    }
+
+    public Bird(String feathers, String layEggs) {
+        this.feathers = feathers;
+        this.layEggs = layEggs;
+    }
+
+
+    public abstract void fly();
+}
